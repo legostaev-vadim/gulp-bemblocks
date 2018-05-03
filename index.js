d09383e127caf62630eb2e80f0bf138acfe78523
Vadim <legostaev.vadim@mail.ru>
2018-05-03 22:31:44 +0300

    edit

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-let set_blocks;
+let lex = require('pug-lexer'),
+    through2 = require('through2').obj,
+    pages = new Map(),
+    blocks = new Map(),
+    mixins;
 
-function bemblocks(tokens) {
-    'use strict';
+
+exports.pages = pages;
+exports.blocks = blocks;
+
+exports.lex = function(type) {
+
+    if (type === 'pages') pages.clear();
+    else if (type === 'blocks') blocks.clear();
+    else return through2(function(file, enc, callback) {callback(null, file)});
     
-    if (this.pages.has(tokens[0].loc.filename)) {
-        set_blocks = this.pages.get(tokens[0].loc.filename).blocks;
-    }
+    return through2(function(file, enc, callback) {
 
-    tokens.forEach(token => {
-        if (token.type === 'call') set_blocks.add(token.val);
+        if (type === 'pages') {
+            pages.set(file.stem, new Set());
+            mixins = pages.get(file.stem);
+        } else if (type === 'blocks') {
+            blocks.set(file.stem, new Set());
+            mixins = blocks.get(file.stem);
+        }
+
+        lex(file.contents.toString()).forEach(token => {
+            if (token.type === 'call') {
+                mixins.add(token.val);
+            }
+        });
+
+        callback(null, file);
     });
-
-    return tokens;
-}
-
-module.exports = {
-    postLex: bemblocks,
-    pages: new Map()
 }
